c2a03f11a7076493001563af693b7cf4c2dbd9f2
Eszter Egyud <eszter.egyud@telus.com>
2022-11-23 15:58:50 -0700

    implemented movement control in Snek game

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 //snek plaj.js
-const { connect } = require("./client.js");
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+// connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function() {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+setupInput(connect());
